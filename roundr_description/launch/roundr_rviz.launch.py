d403ef89c0bc65a20f3d02c1c7aaf2d36671655c
linzhank <linzhank@gmail.com>
2021-10-29 15:34:02 -0500

    switch roundr_description build_type to ament_cmake; complete urdf

--- roundr_description/launch/roundr_rviz.launch.py
+++ roundr_description/launch/roundr_rviz.launch.py
@@ -9,9 +9,9 @@ from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-    urdf_tutorial_path = get_package_share_path('roundr_description')
-    default_model_path = urdf_tutorial_path / 'urdf/roundr.urdf'
-    default_rviz_config_path = urdf_tutorial_path / 'rviz/urdf.rviz'
+    package_share_path = get_package_share_path('roundr_description')
+    default_model_path = str(package_share_path / 'urdf/roundr.urdf')
+    default_rviz_config_path = str(package_share_path / 'rviz/roundr.rviz')
 
     gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
                                     description='Flag to enable joint_state_publisher_gui')
